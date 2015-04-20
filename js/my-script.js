4049e7dda7682f695f2dc9e8aab1c3c6a37ff1eb
Cameron Lambert <lambert.cam21@gmail.com>
2015-04-20 15:26:34 -0600

    Added pictures for chrome webstore, changed extension id

--- js/my-script.js
+++ js/my-script.js
@@ -930,7 +930,7 @@ function DOMCommunicator(){
 
 // 5. EXTENSION COMMUNICATOR
 function monarchExtensionMessenger(){
-	this.extensionId = "jpfdiljpmcaodjjmacglfoighpgnmcoi";
+	this.extensionId = "adpiackmfegmpblfljmjbfahoglbeomh";
 	
 	this.extensionMessageResponse = function(response){
 		if(!response.success){
